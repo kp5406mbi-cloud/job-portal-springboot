79c7ead15b47bfbe57f09b28fc6dc8912f5a3fee
kp5406mbi-cloud <kp5406.mbi@gmail.com>
2026-06-19 03:47:40 +0530

    Google OAuth integration

--- src/main/java/com/jobportal/service/CustomOAuth2UserService.java
+++ src/main/java/com/jobportal/service/CustomOAuth2UserService.java
@@ -8,6 +8,7 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import java.util.UUID;
 
 @Service
 public class CustomOAuth2UserService
@@ -36,7 +37,7 @@ public class CustomOAuth2UserService
 
                     user.setEmail(email);
 
-                    user.setPassword("");
+                    user.setPassword(UUID.randomUUID().toString());
 
                     user.setRole("USER");
 
